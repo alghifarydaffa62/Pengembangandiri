759bfee6d7c82aa0c759028bbab5b3546d3609b3
Muhamad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2024-09-06 16:58:36 +0700

    image gallery done

--- script3.js
+++ script3.js
@@ -1,9 +1,19 @@
 const container = document.querySelector('.container');
 const pertama = document.querySelector('.pertama')
+const thumbs = document.querySelectorAll('.thumb')
 
 container.addEventListener('click', function(e) {
     if(e.target.className == 'thumb') {
         pertama.src = e.target.src
         pertama.classList.add('fade')
     }
+    setTimeout(function() {
+        pertama.classList.remove('fade')
+    }, 500)
+    thumbs.forEach(function(thumb) {
+        if(thumb.classList.contains('active')) {
+            thumb.classList.remove('active')
+        }
+    })
+    e.target.classList.add('active')
 })
